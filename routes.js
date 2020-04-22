d2af4aef388c475a7f1bdbaa27f1841ae0774b4e
RaphaelQSmith <raphaelsmithdublin@gmail.com>
2020-04-22 14:38:40 +0000

    Delete and Select row

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ movieCtrl = require('./movie-controller');
 router.post('/movies', movieCtrl.createMovie);
 router.get('/movies', movieCtrl.getMovies);
 router.get('/movies/:id', movieCtrl.getMovie);
-router.delete('/movies/:id', movieCtrl.deleteMovie);
+router.delete('/movies/del/:id', movieCtrl.deleteMovie);
 router.put('/movies/:id', movieCtrl.updateMovie);
 
 module.exports = router;
--- views/jquery/table.js
+++ views/jquery/table.js
@@ -1,53 +1,72 @@
-function draw_table()
-{
-	$("#results").empty();
-	$.getJSONuncached = function (url)
-	{
-		return $.ajax(
-		{
-			url: url,
-			type: 'GET',
-			cache: false,
-			success: function (html)
-			{
-                $("#results").append(html);
-				select_row();
-			}
-		});
-	};
-	$.getJSONuncached("/movies")
-};
+// function draw_table()
+// {
+// 	$("#results").empty();
+// 	$.getJSONuncached = function (url)
+// 	{
+// 		return $.ajax(
+// 		{
+// 			url: url,
+// 			type: 'GET',
+// 			cache: false,
+// 			success: function (html)
+// 			{
+//                 $("#results").append(html);
+// 				select_row();
+// 			}
+// 		});
+// 	};
+// 	$.getJSONuncached("/movies")
+// };
 
-function select_row()
-{
-	$("#menuTable tbody tr[id]").click(function ()
-	{
-		$(".selected").removeClass("selected");
-		$(this).addClass("selected");
-		var mov = $(this).attr("id") - 1;
-		delete_row(mov);
-	})
-};
+// function select_row()
+// {
+// 	$("#menuTable tbody tr[id]").click(function ()
+// 	{
+// 		$(".selected").removeClass("selected");
+// 		$(this).addClass("selected");
+// 		var mov = $(this).attr("id");
+// 		delete_row(mov);
+// 	})
+// };
 
-function delete_row(mov)
-{
-	$("#delete").click(function ()
-	{
-		$.ajax(
-		{
-			url: "/movies/:id",
-			type: "POST",
-			data:
-			{
-				movie: mov
-			},
-			cache: false,
-			success: setTimeout(draw_table, 1000)
-		})
-	})
-};
+// function delete_row(id){
 
-$(document).ready(function ()
-{
-	draw_table();
-});
+// 	$("#delete").click(function ()
+// 	{
+// 		$.ajax(
+// 		{
+// 			url: "/movies/del/"+id,
+// 			type: "delete",
+// 			data:
+// 			{
+// 				movie: mov
+// 			},
+// 			cache: false,
+// 			success: setTimeout(draw_table, 1000)
+// 		})
+// 	})
+// };
+
+
+
+// $(document).ready(function ()
+// {
+    
+// });
+
+
+// document.getElementById('delete').addEventListener('click', function() {
+//     var tableRef = document.getElementById('links-list');
+//     var tableRows = document.getElementById('links-list').rows;
+
+//     var checkedRows = [];
+//     for (var i = 0; i < tableRows.length; i++) {
+//         if (tableRows[i].querySelector('input').checked) {
+//             checkedRows.push(tableRows[i]);
+//         }
+//     }
+
+//     for (var k = 0; k < checkedRows.length; k++) {
+//         checkedRows[k].parentNode.removeChild(checkedRows[k]);
+//     }
+// });
