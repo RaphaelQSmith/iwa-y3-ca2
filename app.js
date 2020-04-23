6021c0b713c331f8c072467e8933ca3cba12ed37
RaphaelQSmith <raphaelsmithdublin@gmail.com>
2020-04-23 12:28:09 +0000

    "Further view editing"

--- app.js
+++ app.js
@@ -6,25 +6,22 @@ var http = require('http'),
     mongoose = require('mongoose'),
     dotenv = require('dotenv')
     dotenv.config();
-
-// MongoDB connected 
+    
+const exphbs = require('express-handlebars');
 
 var router = express();
 var port = 3000;
+
 var movieCtrl = require('./movie-controller');
 
 router.use(logger('dev'));
-// router.use(bodyParser.json());
+router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 const movieRoute = require('./routes');
 router.use('/', movieRoute);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
-// router.use(express.json());
-// router.use(express.urlencoded({extended: true})); 
-
-
 
 router.listen(port, function(err){
     console.log("Listening on Port: " + port)
@@ -38,86 +35,3 @@ mongoose.connection.on('error', (err) => {
 mongoose.connection.on('connected', () => { 
     console.log('MongoDB is successfully connected');
 });
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function xmlFileToJs(filename, cb) {
-//   var filepath = path.normalize(path.join(__dirname, filename));
-//   fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-//     if (err) throw (err);
-//     xml2js.parseString(xmlStr, {}, cb);
-//   });
-// }
-
-// function jsToXmlFile(filename, obj, cb) {
-//   var filepath = path.normalize(path.join(__dirname, filename));
-//   var builder = new xml2js.Builder();
-//   var xml = builder.buildObject(obj);
-//   fs.unlinkSync(filepath);
-//   fs.writeFile(filepath, xml, cb);
-// }
-
-
-
-
-// router.get('/get/html', function(req,res){
-
-//     res.writeHead(200, {'Content-Type':'text/html'});
-
-//     var xml = fs.readFileSync("movies.xml", 'utf8');
-//     var xsl = fs.readFileSync("movies.xsl", 'utf8');
-    
-//     var doc = xmlParse(xml);
-//     var stylesheet = xmlParse(xsl);
-
-//     var result = xsltProcess(doc, stylesheet);
-
-//     res.end(result.toString());
-
-// });
-
-// router.post('/post/json', function(req, res) {
-
-//   function appendJSON(obj) {
-//     xmlFileToJs('movies.xml', function(err, result) {
-//       if (err) throw (err);
-//       result.movielist.movie.push({'title': obj.title, 'year': obj.year, 'genre': obj.genre, 'cast': obj.cast}); 
-//       jsToXmlFile('movies.xml', result, function(err) {
-//         if (err) console.log(err);
-//       })
-//     })
-//   };
-//   appendJSON(req.body);
-//   res.redirect('back');
-// });
-
-// router.post('/post/delete', function(req, res) {
-
-//     function deleteJSON(obj) {
-    
-//     xmlFileToJs('movies.xml', function(err, result) {
-//       if (err) throw (err);
-//       delete result.movielist.movie[obj.movie];
-//       jsToXmlFile('movies.xml', result, function(err) {
-//         if (err) console.log(err);
-//       })
-//     })
-//   }
-//   deleteJSON(req.body);
-// });
-
--- views/jquery/table.js
+++ views/jquery/table.js
@@ -1,72 +1,53 @@
-// function draw_table()
-// {
-// 	$("#results").empty();
-// 	$.getJSONuncached = function (url)
-// 	{
-// 		return $.ajax(
-// 		{
-// 			url: url,
-// 			type: 'GET',
-// 			cache: false,
-// 			success: function (html)
-// 			{
-//                 $("#results").append(html);
-// 				select_row();
-// 			}
-// 		});
-// 	};
-// 	$.getJSONuncached("/movies")
-// };
+function draw_table()
+{
+	$("#results").empty();
+	$.getJSONuncached = function ('/movies')
+	{
+		return $.ajax(
+		{
+			url: '/movies',
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#results").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/movies")
+};
 
-// function select_row()
-// {
-// 	$("#menuTable tbody tr[id]").click(function ()
-// 	{
-// 		$(".selected").removeClass("selected");
-// 		$(this).addClass("selected");
-// 		var mov = $(this).attr("id");
-// 		delete_row(mov);
-// 	})
-// };
+function select_row()
+{
+	$("#menuTable tbody tr[id]").click(function ()
+	{
+		$(".selected").removeClass("selected");
+		$(this).addClass("selected");
+		var mov = $(this).attr("id") - 1;
+		delete_row(mov);
+	})
+};
 
-// function delete_row(id){
+function delete_row(mov)
+{
+	$("#delete").click(function ()
+	{
+		$.ajax(
+		{
+			url: "/movies/del",
+			type: "POST",
+			data:
+			{
+				movie: mov
+			},
+			cache: false,
+			success: setTimeout(draw_table, 1000)
+		})
+	})
+};
 
-// 	$("#delete").click(function ()
-// 	{
-// 		$.ajax(
-// 		{
-// 			url: "/movies/del/"+id,
-// 			type: "delete",
-// 			data:
-// 			{
-// 				movie: mov
-// 			},
-// 			cache: false,
-// 			success: setTimeout(draw_table, 1000)
-// 		})
-// 	})
-// };
-
-
-
-// $(document).ready(function ()
-// {
-    
-// });
-
-
-// document.getElementById('delete').addEventListener('click', function() {
-//     var tableRef = document.getElementById('links-list');
-//     var tableRows = document.getElementById('links-list').rows;
-
-//     var checkedRows = [];
-//     for (var i = 0; i < tableRows.length; i++) {
-//         if (tableRows[i].querySelector('input').checked) {
-//             checkedRows.push(tableRows[i]);
-//         }
-//     }
-
-//     for (var k = 0; k < checkedRows.length; k++) {
-//         checkedRows[k].parentNode.removeChild(checkedRows[k]);
-//     }
-// });
+$(document).ready(function ()
+{
+	draw_table();
+});
